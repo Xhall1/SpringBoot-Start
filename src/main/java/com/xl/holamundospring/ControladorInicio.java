41d9f2fa64c5b30f51d1f34b15e195266ce88595
Xhall1 <gridoracones@gmail.com>
2025-07-14 22:53:05 -0500

    sharing information with Spring MVC and Thymeleaf

--- src/main/java/com/xl/holamundospring/ControladorInicio.java
+++ src/main/java/com/xl/holamundospring/ControladorInicio.java
@@ -1,7 +1,9 @@
 package com.xl.holamundospring;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class ControladorInicio {
 
+    @Value("${index.greetings}")
+    private String greetings;
+
     @GetMapping("/")
-    public String inicio(){
+    public String inicio(Model model) {
+        var message = "Message with Thymeleaf";
         log.info("running the controller Spring MVC");
+        model.addAttribute("message", message);
+        model.addAttribute("greetings", greetings);
         return "index";
     }
 }
